99bcf5abe744edbcd8d5a03a0324f41176ccdd8c
nguyenvanquy22 <nguyenvanquy9a2003@gmail.com>
2023-04-24 18:38:47 +0700

    update page number history

--- src/module/Menu.java
+++ src/module/Menu.java
@@ -16,10 +16,11 @@ import connection.GameDao;
 public class Menu extends JPanel implements KeyListener 
 {	
 	private int level = 0;
-	private double valueFont[] = {1.5,1,1};
+	private double valueFont[] = {1.5,1,1,1};
 	private Image backGround = Toolkit.getDefaultToolkit().getImage("src/images/bgMenu.jpg");
 	
-	private int pageIndex = 1;
+	private int pageIndexCurrent = 1;
+	private int pageIndexMax = 3;
 	private int pageNumber;
 	
 	private boolean menuDisplay = true;
@@ -76,9 +77,8 @@ public class Menu extends JPanel implements KeyListener
 			g.drawString("Hard (Press 2)", 250, 350);
 			
 			// display history
-			g.setColor(Color.red);
-			g.setFont(new Font("serif",Font.CENTER_BASELINE, 25));
-			g.drawString("History Play", 250, 400);
+			g.setFont(new Font("serif",Font.CENTER_BASELINE, (int) (20*valueFont[3])));
+			g.drawString("History Play ( H )", 250, 400);
 		} 
 		else 
 		{
@@ -102,27 +102,28 @@ public class Menu extends JPanel implements KeyListener
 			}
 			int n = data.size();
 			pageNumber = n%10==0 ? n%10:n%10+1;
-			pageNumber = pageNumber>3 ? 3:pageNumber;
+			pageNumber = pageNumber>pageIndexMax ? pageIndexMax:pageNumber;
 			
 			g.setColor(Color.white);
 			g.setFont(new Font("serif",Font.CENTER_BASELINE, 20));
-			int t = (pageIndex-1)*10;
-			int x = 1;
-			for(int i=n-t-1; i>=n-10-t; i--) 
+			int t = (pageIndexCurrent-1)*10;
+			int No = 1;
+			for(int i=n-t-1; i>=n-10-t && i>=0; i--) 
 			{
-				g.drawString((x+t)<10?"0"+(x+t):""+(x+t), 55, 50 + 42*x);
-				g.drawString(data.get(i).getTimePlayGame(), 150, 50 + 42*x);
-				g.drawString(data.get(i).getLevelGame()+"", 400, 50 + 42*x);
-				g.drawString(data.get(i).getScore()+"", 490, 50 + 42*x);
-				g.drawString(data.get(i).getResult()==1?"Win":"Lose", 590, 50 + 42*x);
-				x++;
+				int y = 50+42*No;
+				g.drawString((No+t)<10?"0"+(No+t):""+(No+t), 55, y);
+				g.drawString(data.get(i).getTimePlayGame(), 150, y);
+				g.drawString(data.get(i).getLevelGame()+"", 400, y);
+				g.drawString(data.get(i).getScore()+"", 490, y);
+				g.drawString(data.get(i).getResult()==1?"Win":"Lose", 590, y);
+				No++;
 			}
 			
 			// prev & next
 			g.setColor(Color.red);
 			g.setFont(new Font("serif",Font.CENTER_BASELINE, 30));
 			g.drawString("<--", 500, 535);
-			g.drawString("0"+pageIndex, 555, 535);
+			g.drawString("0"+pageIndexCurrent, 555, 535);
 			g.drawString("-->", 600, 535);			
 		}
 		
@@ -144,11 +145,15 @@ public class Menu extends JPanel implements KeyListener
 					break;
 				case KeyEvent.VK_DOWN:
 					valueFont[level++] = 1;
-					level = level>2 ? 2:level;
+					level = level>3 ? 3:level;
 					valueFont[level] = 1.5;
 					break;
 				case KeyEvent.VK_ENTER:
-					menuDisplay = false;
+					if(level<3) menuDisplay = false;
+					else {
+						historyDisplay = true;
+						getDataOnece = true;
+					}
 					break;
 				case KeyEvent.VK_0:
 					level = 0;
@@ -178,12 +183,12 @@ public class Menu extends JPanel implements KeyListener
 					historyDisplay = false;
 					break;
 				case KeyEvent.VK_LEFT:
-					pageIndex--;
-					pageIndex = pageIndex<1 ? 1:pageIndex;
+					pageIndexCurrent--;
+					pageIndexCurrent = pageIndexCurrent<1 ? 1:pageIndexCurrent;
 					break;
 				case KeyEvent.VK_RIGHT:
-					pageIndex++;
-					pageIndex = pageIndex>3 ? 3:pageIndex;
+					pageIndexCurrent++;
+					pageIndexCurrent = pageIndexCurrent>pageNumber ? pageNumber:pageIndexCurrent;
 					break;
 				default:
 					break;
